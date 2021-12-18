cca0cced9976ace8c6016c1e21f96e5f2723c29a
Rob Friesel <rob@founddrama.net>
2021-12-17 21:19:35 -0500

    experiment: generate masthead from frontmatter

--- main.ts
+++ main.ts
@@ -1,137 +1,85 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownPostProcessorContext } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const toFixed = (source: string, significantDigits: number): string => (
+  Number.parseFloat(source).toFixed(significantDigits)
+);
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+type BjcpStat = {
+  low?: string;
+  high?: string;
+  flexible?: boolean;
+};
+const renderStatRange = (
+  stat: BjcpStat,
+  significantDigits: number = 0,
+  suffix: string = ''
+): string => {
+  if (stat.flexible === true) {
+    return '<em>Varies</em>';
+  } else {
+    return `${toFixed(stat.low, significantDigits)}-${toFixed(stat.high, significantDigits)}${suffix}`;
+  }
+};
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class BjcpPlugin extends Plugin {
+  async onload() {
+    this.registerMarkdownPostProcessor((el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+      console.log('el => ', el);
+      console.log('ctx => ', ctx);
+      const { frontmatter } = ctx;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+      if (!frontmatter.style || !frontmatter.style_name || !frontmatter.stats) {
+        return;
+      }
 
-	async onload() {
-		await this.loadSettings();
+      let frontMatterContainer = el.querySelector('.frontmatter-container');
+      if (frontMatterContainer !== null) {
+        const { stats } = frontmatter;
+        const isBeer = frontmatter.type === 'beer';
+        const cssClass = isBeer ? 'bjcp-stats-beer-cell' : 'bjcp-stats-cell';
+        
+        let frontMatterFancy = document.createElement('div');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        const beerOnlyCells = `
+<td class="${cssClass}">
+  ${renderStatRange(stats.ibu)}
+  <br/><small>IBU</small>
+</td>
+<td class="${cssClass}">
+  ${renderStatRange(stats.srm)}
+  <br/><small>SRM</small>
+</td>
+`;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        frontMatterFancy.innerHTML = `
+<h1>${frontmatter.style}. ${frontmatter.style_name}</h1>
+<table class="bjcp-stats-table">
+  <tr>
+    <td colspan="5">
+      Vital Statistics
+    </td>
+  </tr>
+  <tr>
+    ${isBeer ? beerOnlyCells : ''}
+    <td class="${cssClass}">
+      ${renderStatRange(stats.og, 3)}
+      <br/><small>O.G.</small>
+    </td>
+    <td class="${cssClass}">
+      ${renderStatRange(stats.fg, 3)}
+      <br/><small>F.G.</small>
+    </td>
+    <td class="${cssClass}">
+      ${renderStatRange(stats.abv, 1, '%')}
+      <br/><small>ABV</small>
+    </td>
+  </tr>
+</table>
+<p>Examples: ${frontmatter.examples.join(', ')}</p>
+        `;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        el.appendChild(frontMatterFancy);
+      }
+    });
+  }
 }
